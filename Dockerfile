93fe7ad762ec840e48589c34adfa88ebdc4dd33e
Artem-ne-Artem <ddd.z.2000@yandex.ru>
2025-11-30 18:51:15 +0300

    del dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
-FROM apache/airflow:2.10.5
-COPY requirements.txt .
-RUN pip install --no-deps -r requirements.txt
+
+# FROM apache/airflow:2.10.5
+# COPY requirements.txt .
+# RUN pip install --no-deps -r requirements.txt
--- airflow/dags/config/postgres_config.py
+++ airflow/dags/config/postgres_config.py
@@ -2,7 +2,7 @@ import os
 
 
 DB_CONN = {
-    "host": "postgres_db_dwh",
+    "host": "swapi_postgres_db_dwh",
     "port": 5432,
     "user": os.getenv("POSTGRES_DWH_USER"),
     "password": os.getenv("POSTGRES_DWH_PASSWORD"),
